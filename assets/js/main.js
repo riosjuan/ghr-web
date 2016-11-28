4b499fa8fb099d96ccf7646183b264eca11f87ea
riosjuan <rios@ngti.nl>
2016-11-28 23:29:41 +0100

    Added location dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,87 +1,28 @@
 /**
- * Created by juanrios on 25/11/2016.
+ * Created by Juan Rios on 25/11/2016.
  */
-$(document).ready(function(){
 
-    // check for Geolocation support
-    if (navigator.geolocation) {
-        console.log('Geolocation is supported!');
-    }
-    else {
-        console.log('Geolocation is not supported for this Browser/OS.');
-    }
+function parse(xml){
 
-    window.onload = function() {
-        var startPos;
-        var geoSuccess = function(position) {
-            startPos = position;
-            document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-            document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-        };
-        navigator.geolocation.getCurrentPosition(geoSuccess);
-        console.log();
-    };
+    xmlDoc = $.parseXML(xml),
+        $xml = $( xmlDoc );
 
-
-    /*Background color*/
-
-    ///////////////////////////
-
-    var now = new Date(Date.now());
-    formatted = now.getHours();
-
-    if (formatted > 17) { // Night
-        $('body').removeClass();
-        $('body').addClass("night-gradient");
-    } else { // Day
-        $('body').removeClass();
-        $('body').addClass("day-gradient");
-    }
-
-    ///////////////////////////
-
-
-
-    function parse(xml){
-
-        var xmlDoc = $.parseXML( xml ),
-            $xml = $( xmlDoc );
-
-        var currentTemp = $xml.find("weerstation#6344 > temperatuurGC"); // Current temperature
-        currentDate = $xml.find("weerstation#6344 > datum"); // Current date
-        city = $xml.find("weerstation#6344 > stationnaam").text();
-        pressure = $xml.find("weerstation#6344 > luchtdruk"); // Current atmospheric pressure
-        rain = $xml.find("weerstation#6344 > regenMMPU").text(); // Rain index
-        bar = 10;
-        pressureBar = pressure.text() / bar; // Converts hPa to kPa
-
-        console.log(pressureBar);
-        console.log(pressureBar > 100.914 && pressureBar < 101.9148 && rain > 0);
-        console.log(rain);
-        console.log(city);
-
-        $("#temperature").html(Math.round(currentTemp.text())).append("&#x2103;");
-        $("#date").html(currentDate).prepend("Laaste update: ");
-
-        if (pressureBar < 100.914) {
-            $("#forecast").html("storm");
-        } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain < 0.05) { //102.269
-            $("#forecast").html("it might rain");
-        } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain < 2.5) {
-            $("#forecast").html("Light rain");
-        } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 2.5 && rain < 7.6  ) {
-            $("#forecast").html("Moderate rain");
-        }  else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 7.6 && rain < 50  ) {
-            $("#forecast").html("Heavy rain");
-        } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 50  ) {
-            $("#forecast").html("Yes");
-            $("#forecast-sub").html("There is violent rain expected in the next two hours");
-        } else {
-            $("#forecast").html("No");
-            $("#forecast-sub").html("There is not rain expected in the next two hours");
+    stationLoc = $xml.find("weerstations > weerstation").map(function() {
+        return  {
+            name: $(this).find("stationnaam").text(),
+            id:  $(this).find("stationcode").text()
         }
-    }
+    }).get();
 
+    $.each(stationLoc, function(key, value) {
+        $("#stations")
+            .append($("<option></option>")
+                .attr("value", value.id)
+                .text(value.name));
+    });
+}
+
+$(document).ready(function(){
 
     $.ajax({
         url: 'https://xml.buienradar.nl/', // name of file you want to parse
@@ -89,4 +30,59 @@ $(document).ready(function(){
         success: parse,
         error: function(){alert("Error: Oops! something went wrong");}
     });
+
+    /*Background color*/
+
+    var now = new Date(Date.now());
+        formatted = now.getHours();
+
+    if (formatted > 17) {
+        $('body').removeClass().addClass("night-gradient"); // Night
+    } else {
+        $('body').removeClass().addClass("day-gradient"); // Day
+    }
 });
+
+function selectionLoc() {
+
+    $("#infoCard").fadeIn(500);
+
+    // var selectedLoc = $("#stations :selected").text();
+        selectedID = $("#stations :selected").val();
+
+    var currentTemp = $xml.find("weerstation#" + selectedID + "> temperatuurGC"); // Current temperature
+        currentDate = $xml.find("weerstation#" + selectedID + "> datum"); // Current date
+        locatie = $xml.find("weerstation#"+ selectedID + "> stationnaam").text();
+        pressure = $xml.find("weerstation#" + selectedID + "> luchtdruk"); // Current atmospheric pressure
+        rain = $xml.find("weerstation#" + selectedID + "> regenMMPU").text(); // Rain index
+        bar = 10;
+        pressureBar = pressure.text() / bar; // Converts hPa to kPa
+
+    console.log(pressureBar);
+    console.log(pressureBar > 100.914 && pressureBar < 101.9148 && rain > 0);
+    console.log(rain);
+
+    $("#temperature").html(Math.round(currentTemp.text())).append("&#x2103;");
+    $("#date").html(currentDate).prepend("Laaste update: ");
+    $("#station").html(locatie);
+
+
+    if (pressureBar < 100.914) {
+        $("#forecast").html("storm");
+    } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain < 0.05) { //102.269
+        $("#forecast").html("it might rain");
+    } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain < 2.5) {
+        $("#forecast").html("Light rain");
+    } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 2.5 && rain < 7.6  ) {
+        $("#forecast").html("Moderate rain");
+    }  else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 7.6 && rain < 50  ) {
+        $("#forecast").html("Heavy rain");
+    } else if (pressureBar > 100.914 && pressureBar < 101.9148 && rain > 50  ) {
+        $("#forecast").html("Yes");
+        $("#forecast-sub").html("There is violent rain expected in the next two hours");
+    } else {
+        $("#forecast").html("No");
+        $("#forecast-sub").html("There is not rain expected in the next two hours");
+    }
+
+}
